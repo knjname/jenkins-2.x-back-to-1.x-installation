4dc6d990fd6d225f05e593f1ecb8bb877d5d022e
System Administrator <root@knjname201407MBP.local>
2016-07-20 01:12:01 +0900

    Initial add

---
+++ disableSecurity.groovy
@@ -0,0 +1,12 @@
+import hudson.model.*
+import jenkins.model.*
+
+Thread.start {
+    sleep 10000
+    File securityInitFile = new File(Jenkins.instance.rootDir, 'badass-security-init')
+    if(! securityInitFile.exists()) {
+        Jenkins.instance.disableSecurity()
+        securityInitFile.setText('initialized', 'UTF-8')
+    }
+}
+				
